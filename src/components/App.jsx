bc1db1a5f7a92f1671e4dd7658f2b465ea6a6013
internadine <nadine.wischmeier@outlook.com>
2022-02-07 20:43:59 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+    return <div className="container">
+        <div className="heading">
+            <h1>Nadine's ToDo's</h1>
+        </div>
+        <div className="form" >
+            <input type="text" />
+            <button><span>Add</span></button>
+        </div>
+    </div>
+}
+
+export default App;
